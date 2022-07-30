547ad72f7814608653c4df584b6c5370348df4ed
mohsenfarhangi <31503523+mohsenfarhangi@users.noreply.github.com>
2022-07-30 18:20:20 +0430

    Update EnqueueScripts.php

--- admin/classes/EnqueueScripts.php
+++ admin/classes/EnqueueScripts.php
@@ -13,9 +13,9 @@ class EnqueueScripts
 
     public function adminScripts()
     {
-        wp_enqueue_style('main-admin', FDUP_URL . '/assets/css/admin-style.css');
+        wp_enqueue_style('main-admin', SWPS_URL . '/assets/css/admin-style.css');
 
-        wp_enqueue_script('main-admin', FDUP_URL . '/assets/js/admin-main.js', ['jquery', 'fdup-datatable'], false, true);
+        wp_enqueue_script('main-admin', SWPS_URL . '/assets/js/admin-main.js', ['jquery', 'fdup-datatable'], false, true);
         wp_localize_script('main-admin', 'object_name', [
             'loader_gif' => SWPS_URL . '/assets/media/loader-200px.gif',
             'assets_url' => SWPS_URL . './assets/'
